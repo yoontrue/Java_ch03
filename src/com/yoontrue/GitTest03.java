5bade1aefcc70891301dd6c8bf9b25fc849afd4a
yoontrue <yoontrue92@gmail.com>
2021-01-21 12:36:50 +0900

    세컨드 브랜치에서 두번째 푸시

---
+++ src/com/yoontrue/GitTest03.java
@@ -0,0 +1,13 @@
+package com.yoontrue;
+
+public class GitTest03 {
+
+	public static void main(String[] args) {
+		System.out.println("깃 병합 해볼 예정.");
+		System.out.println("세컨드 브랜치로 파일추가 후 ");
+		System.out.println("세컨드 브랜치로 푸시하고 ");
+		System.out.println("마스터 브랜치로 체크아웃하고 ");
+		System.out.println("마스터로 다시 푸시해보기 ");
+	}
+
+}
